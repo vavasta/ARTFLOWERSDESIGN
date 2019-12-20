83930349490d3032efc6497cef49febec8cb48c3
Val <vavasta96@gmail.com>
2019-12-20 14:40:45 +0200

    add new menu styles

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
-const firstname = document.getElementById(firstname);
-const lastname = document.getElementById(lastname);
-const email = document.getElementById(email);
-const telephone = document.getElementById(telephone);
+const firstname = document.getElementById("firstname");
+const firstnameerror = document.getElementById("firstname__error");
+const lastname = document.getElementById("lastname");
+const lastnameerror = document.getElementById("lastname__error");
+const email = document.getElementById("email");
+const emailerror = document.getElementById("email__error");
+const telephone = document.getElementById("telephone");
+const telephoneerror = document.getElementById("telephone__error");
+const text = document.getElementById("text");
+const texterror = document.getElementById("text__error");
+const form = document.querySelector("form");
+const error = document.getElementById("bottomerror");
+const fielderror = document.getElementById("form__required");
+////
 
-function InputValidation() {
-  const x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("PNES");
-    return false;
+form.addEventListener("submit", ValidationEvent);
+
+function ValidationEvent(e) {
+  e.preventDefault();
+  FirstNameValidation(e);
+  LastNameValidation(e);
+  EmailValidation(e);
+  TelephoneValidation(e);
+  TextValidation(e);
+}
+
+function FirstNameValidation(e) {
+  e.preventDefault();
+  if (firstname.value == "") {
+    firstnameerror.style.display = "block";
+  }
+}
+
+function LastNameValidation(e) {
+  e.preventDefault();
+  if (lastname.value == "") {
+    lastnameerror.style.display = "block";
+  }
+}
+function EmailValidation(e) {
+  e.preventDefault();
+  if (email.value == "") {
+    emailerror.style.display = "block";
+  }
+}
+function TelephoneValidation(e) {
+  e.preventDefault();
+  if (telephone.value == "") {
+    telephoneerror.style.display = "block";
+  }
+}
+function TextValidation(e) {
+  e.preventDefault();
+  if (text.value == "") {
+    texterror.style.display = "block";
   }
 }
